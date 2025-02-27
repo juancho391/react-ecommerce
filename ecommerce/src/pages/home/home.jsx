6791f4d52761b1362948b24dc41d4302c32889a7
juancho391 <juanchischis2010@hotmail.com>
2025-02-27 17:55:58 -0500

    Añadi props al componente card en lugar de tener valores fijos

--- ecommerce/src/pages/home/home.jsx
+++ ecommerce/src/pages/home/home.jsx
@@ -5,7 +5,20 @@ import { Card } from "../../components/Card/Card";
 function Home() {
   return (
     <Layout>
-      <Card />
+      <Card
+        imageUrl={
+          "https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=400"
+        }
+        productName={"HeadPhones"}
+        productPrice={1000}
+      />
+      <Card
+        imageUrl={
+          "https://images.pexels.com/photos/437037/pexels-photo-437037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+        productName={"Apple Watch"}
+        productPrice={500}
+      />
     </Layout>
   );
 }
